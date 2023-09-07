caa866fea6e395d25aca7ef8cfb70333ae7ad95f
Gabriela Avila <gabrielanicoleavila@icloud.com>
2023-09-07 09:46:53 -0500

    2nd

--- TempApp/ViewController.swift
+++ TempApp/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 class ViewController: UIViewController {
 //hello'
     
+    @IBOutlet weak var TempTFOutlet: UITextField!
+    
+    @IBOutlet weak var UnitOutlet: UITextField!
+    
+    @IBOutlet weak var LabelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func KButton(_ sender: UIButton) {
+        var temp = Int(TempTFOutlet.text!)
+        var unit = UnitOutlet.text
+        if(unit == "K")
+        {
+            LabelOutlet.text = "\(temp)"
+        }else if(unit == "F")
+        {
+            temp = (temp! - 32) * 5
+            temp = temp! / 9
+            temp = temp! + 273
+            
+        }
+    }
+    
+    @IBAction func FButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func CButton(_ sender: UIButton) {
+    }
+    
+    
 
 }
 
