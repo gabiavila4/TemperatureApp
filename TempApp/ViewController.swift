649d37948485613958f85c2ccbaa9990f5f389fb
Gabriela Avila <gabrielanicoleavila@icloud.com>
2023-09-12 09:48:40 -0500

    4th

--- TempApp/ViewController.swift
+++ TempApp/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 //hello'
     
     @IBOutlet weak var TempTFOutlet: UITextField!
@@ -18,105 +18,131 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        TempTFOutlet.resignFirstResponder()
+        UnitOutlet.resignFirstResponder()
         // Do any additional setup after loading the view.
     }
 
     
     @IBAction func KButton(_ sender: UIButton) {
-        var temp = Double(TempTFOutlet.text!)
-        var unit = UnitOutlet.text
-        var tempN = temp
-        
-        if(unit == "K")
-        {
-            
-            LabelOutlet.text = "\(tempN!)"
-            
-        }else if(unit == "F")
-        {
-            tempN = (tempN! - 32)
-            tempN = tempN! * 5
-            tempN = tempN! / 9
-            tempN = (Double(tempN!) + 273.15)
-            LabelOutlet.text = "\(tempN!)"
-            
-            print("Yes F")
-        }else if(unit == "C")
-        {
-            tempN = tempN! + 273.15
-            LabelOutlet.text = "\(tempN!)"
-            
-        }else
+        if (TempTFOutlet.text == "")
         {
             LabelOutlet.text = "TRY AGAIN"
+        }else
+        {
+            var temp = Double(TempTFOutlet.text!)
+            var unit = UnitOutlet.text
+            var tempN = temp
+            
+            if(unit == "K")
+            {
+                
+                LabelOutlet.text = "\(tempN!)"
+                
+            }else if(unit == "F")
+            {
+                tempN = (tempN! - 32)
+                tempN = tempN! * 5
+                tempN = tempN! / 9
+                tempN = (Double(tempN!) + 273.15)
+                var roundedValue = round(tempN! * 100)/100.0
+                LabelOutlet.text = "\(roundedValue)"
+                
+                print("Yes F")
+            }else if(unit == "C")
+            {
+                tempN = tempN! + 273.15
+                var roundedValue = round(tempN! * 100)/100.0
+                LabelOutlet.text = "\(roundedValue)"
+                
+            }else
+            {
+                LabelOutlet.text = "TRY AGAIN"
+            }
+            
         }
-        
-        
         
     }
     
     @IBAction func FButton(_ sender: UIButton) {
         
-        var temp = Double(TempTFOutlet.text!)
-        var unit = UnitOutlet.text
-        var tempN = temp
-        
-        if(unit == "F")
-        {
-            
-            LabelOutlet.text = "\(tempN!)"
-            
-        }else if(unit == "C")
-        {
-            tempN = tempN! * 9
-            tempN = tempN! / 5
-            tempN = (Double(tempN!) + 32)
-            LabelOutlet.text = "\(tempN!)"
-            //print("Yes F")
-        }else if(unit == "K")
-        {
-            tempN = tempN! - 273.15
-            tempN = tempN! * 9
-            tempN = tempN! / 5
-            tempN = tempN! + 32
-            LabelOutlet.text = "\(tempN!)"
-            
-        }else
-        {
-            LabelOutlet.text = "TRY AGAIN"
-        }
-        
-        
+            if (TempTFOutlet.text == "")
+            {
+                LabelOutlet.text = "TRY AGAIN"
+            }else
+            {
+                
+                var temp = Double(TempTFOutlet.text!)
+                var unit = UnitOutlet.text
+                var tempN = temp
+                
+                if(unit == "F")
+                {
+                    
+                    LabelOutlet.text = "\(tempN!)"
+                    
+                }else if(unit == "C")
+                {
+                    tempN = tempN! * 9
+                    tempN = tempN! / 5
+                    tempN = (Double(tempN!) + 32)
+                    var roundedValue = round(tempN! * 100)/100.0
+                    LabelOutlet.text = "\(roundedValue)"
+                    //print("Yes F")
+                }else if(unit == "K")
+                {
+                    tempN = tempN! - 273.15
+                    tempN = tempN! * 9
+                    tempN = tempN! / 5
+                    tempN = tempN! + 32
+                    var roundedValue = round(tempN! * 100)/100.0
+                    LabelOutlet.text = "\(roundedValue)"
+                    
+                }else
+                {
+                    LabelOutlet.text = "TRY AGAIN"
+                }
+                
+            }
     }
     
     @IBAction func CButton(_ sender: UIButton) {
-        var temp = Double(TempTFOutlet.text!)
-        var unit = UnitOutlet.text
-        var tempN = temp
-        
-        if(unit == "C")
+       
+            if (TempTFOutlet.text == "")
+            {
+                LabelOutlet.text = "TRY AGAIN"
+            }else
         {
-            
-            LabelOutlet.text = "\(tempN!)"
-            
-        }else if(unit == "F")
-        {
-            tempN = tempN! - 32
-            tempN = tempN! * 5
-            tempN = (Double(tempN!) / 9)
-            LabelOutlet.text = "\(tempN!)"
-            //print("Yes F")
-        }else if(unit == "K")
-        {
-            tempN = tempN! - 273.15
-            LabelOutlet.text = "\(tempN!)"
-            
-        }else
-        {
-            LabelOutlet.text = "TRY AGAIN"
-        }
-        
-        
+                var temp = Double(TempTFOutlet.text!)
+                var unit = UnitOutlet.text
+                var tempN = temp
+                
+                if(unit == "C")
+                {
+                    
+                    LabelOutlet.text = "\(tempN!)"
+                    
+                }else if(unit == "F")
+                {
+                    tempN = tempN! - 32
+                    tempN = tempN! * 5
+                    tempN = (Double(tempN!) / 9)
+                    var roundedValue = round(tempN! * 100)/100.0
+                    
+                    LabelOutlet.text = "\(roundedValue)"
+                    //print("Yes F")
+                }else if(unit == "K")
+                {
+                    tempN = tempN! - 273.15
+                    var roundedValue = round(tempN! * 100)/100.0
+                    LabelOutlet.text = "\(roundedValue)"
+                    
+                }else
+                {
+                    LabelOutlet.text = "TRY AGAIN"
+                }
+                
+            }
     }
     
     
